fabb2bff69ebf83d3ab74373377d07304bdb4d56
ZAKA-6X <89934286+ZAKA-6X@users.noreply.github.com>
2024-02-29 22:58:52 +0100

    Add files via upload

---
+++ list-set-tuple-dictionary.py
@@ -0,0 +1,21 @@
+# collection = single 'variable' used to store multiple valiues
+# list = [] : ordered and changeable. Duplicates OK
+# set = {} : unordered and immutable, but Add / Remove OK. No duplication
+# tuple = () : ordered and unchangeable. Duplication OK. FASTER
+# dictionary = {key: value} : ordered and changeable. No Duplicates
+
+list = ['apple', 'banana', 'orange', 'pineapple']
+set = {'apple', 'banana', 'orange', 'pineapple'}
+tuple = ('apple', 'banana', 'orange', 'pineapple')
+dictionary = {'Morocco': 'Rabat', 
+              'China': 'Beijing',
+              'Russia': 'Mossow'}
+
+
+# help(collection)
+# dir(collection) 
+
+
+
+
+
